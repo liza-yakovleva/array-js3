18aada543a00d31cc5471eca47d9bb1094ff4843
Elizaveta <lizayakovleva2005@gmail.com>
2020-12-03 17:17:51 +0200

    added all

---
+++ app.js
@@ -0,0 +1,17 @@
+
+// TODO Завдання 3. Отримати від користувача набір чисел через кому, отримати число яке треба знайти.Перевірити чи є це число в масиві ?
+
+let num = prompt("Введіть числа через кому?", "")
+let searchNum = prompt("Введіть шукане число?", "")
+let arr = num.split(",")
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] == searchNum) {
+    console.log(`Максимальне число ${searchNum} є в масиві`)
+    break;
+  }
+  else {
+    console.log(`Числа ${searchNum} НЕ має в масиві`);
+  }
+}
+
+
